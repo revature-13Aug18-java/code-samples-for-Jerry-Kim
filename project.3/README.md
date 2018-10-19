ca5770ca98eebd2c926c96dc8b2f6bc7fcaacbfa
jbki <jbki@protonmail.com>
2018-10-19 00:27:18 -0400

    p3

---
+++ project.3/README.md
@@ -0,0 +1,42 @@
+# RideForce User Service
+Core component in the microservice based app that defines the models and services data directly associated with the _user_, that is, the consumer. This service component allows for:
+  - authenticating users 
+  - checking for access permissions 
+  - interfacing with the persistence layer 
+  - handling exceptions
+
+The readme for this microservice is located at /README.md.old. Instructions are written to ease future developers into the setup, which are environment variables, database and dependency.
+
+Logging is done at three different levels. Output is to the console and to files, which are filtered for root level and service level log statements. It will be helpful to include code snippet here to understand how messages at the error level are logged, which happens whenever an exception is thrown. To reduce code duplication that would happen by inserting a logger inside every catch block, a custom class, `CustomException`, which extends `Exception`, was made to log the message inside the constructor, intercepting the child class's call to `super`:
+
+```java
+// CustomException.java
+public abstract class CustomException extends Exception {
+  private static Logger log = //...
+  public CustomException(String msg) {
+    super(msg);
+    log.error(msg, this);
+  }
+}
+```
+```java
+// EmptyPasswordException.java
+public class EmptyPasswordException extends CustomException {
+  public EmptyPasswordException() {
+    super("Can not set user's password to an empty password");
+  }
+}
+```
+`this` is passed as an argument in order to print the stack trace. For illustration, only included are these two Exception classes as a part of this showcase. 
+
+An authentication token is created using auth0 api: 
+```java
+    return JWT
+      .create()
+      .withIssuer(ISSUER)
+      .withSubject(subject)
+      .withIssuedAt(Date.from(now))
+      .withExpiresAt(Date.from(expires))
+      .sign(algorithm);
+```
+
---
+++ project.3/src/main/java/com/revature/rideforce/user/services/AuthenticationService.java
@@ -0,0 +1,114 @@
+package com.revature.rideforce.user.services;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.revature.rideforce.user.beans.User;
+import com.revature.rideforce.user.beans.UserCredentials;
+import com.revature.rideforce.user.beans.UserRegistrationInfo;
+import com.revature.rideforce.user.exceptions.DisabledAccountException;
+import com.revature.rideforce.user.exceptions.EmptyPasswordException;
+import com.revature.rideforce.user.exceptions.EntityConflictException;
+import com.revature.rideforce.user.exceptions.InvalidCredentialsException;
+import com.revature.rideforce.user.exceptions.InvalidRegistrationKeyException;
+import com.revature.rideforce.user.exceptions.PasswordRequirementsException;
+import com.revature.rideforce.user.exceptions.PermissionDeniedException;
+import com.revature.rideforce.user.repository.UserRepository;
+import com.revature.rideforce.user.security.LoginTokenProvider;
+import com.revature.rideforce.user.security.RegistrationTokenProvider;
+
+import java.lang.invoke.MethodHandles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The service used to handle authentication, that is, logging in, creating new
+ * users, getting information about the current user.
+ */
+@Service
+public class AuthenticationService {
+  static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private LoginTokenProvider loginTokenProvider;
+
+	@Autowired
+	private RegistrationTokenProvider registrationTokenProvider;
+
+	/**
+	 * Authenticates a user with the given credentials, returning a JSON Web Token
+	 * that can be used for future authentication.
+	 * 
+	 * @param credentials the user's credentials (email and password)
+	 * @return a JWT for future authentication
+	 * @throws InvalidCredentialsException if the given credentials are invalid
+	 *                                     (wrong email or password)
+	 * @throws DisabledAccountException	   if the account is disabled (by admin)
+	 */
+	public String authenticate(UserCredentials credentials) throws InvalidCredentialsException, DisabledAccountException {
+		User found = userRepository.findByEmail(credentials.getEmail());
+		log.info("Authenticating user credentials");
+		log.debug("credentials.email(): {} ", credentials.getEmail()); //find solution for logging sensitive data; possibly dbappender
+		log.debug("credentials.password: {} ", credentials.getPassword());
+		if (found == null) {
+			throw new InvalidCredentialsException();
+		}
+		if (!passwordEncoder.matches(credentials.getPassword(), found.getPassword())) {
+			throw new InvalidCredentialsException();
+		}
+		if (found.isActive().equals("DISABLED")) {
+			throw new DisabledAccountException();
+		}
+		return loginTokenProvider.generateToken(found.getId());
+	}
+
+	/**
+	 * Registers a new user with the given information.
+	 * 
+	 * @param info the registration information for the new user
+	 * @return the user that was created
+	 * @throws InvalidRegistrationKeyException if the provided registration key was
+	 *                                         invalid
+	 * @throws EntityConflictException         if the given email is already used by
+	 *                                         another user
+	 * @throws PermissionDeniedException       if the currently logged-in user does
+	 *                                         not have permission to create the
+	 *                                         desired user (e.g. if an
+	 *                                         unauthenticated user attempts to
+	 *                                         create an admin)
+	 * @throws EmptyPasswordException          Password in the {@linkplain UserRegistrationInfo} must be non empty
+	 * @throws PasswordRequirementsException   if the password entered does not
+	 * 										   meet the requirements specified
+	 */
+	public User register(UserRegistrationInfo info)
+			throws InvalidRegistrationKeyException, EntityConflictException, PermissionDeniedException, EmptyPasswordException, PasswordRequirementsException {
+		if(info == null) {
+			throw new InvalidRegistrationKeyException();
+		}
+		// Make sure that the registration key token is valid.
+		if (!registrationTokenProvider.isValid(info.getRegistrationKey())) {
+			log.info("Attempting to register user");
+			log.debug(info.getRegistrationKey());
+			throw new InvalidRegistrationKeyException();
+		}
+		// Make sure password meets requirements.
+		if(!passwordIsValid(info.getPassword())) {
+			log.info("Password length violation");
+			throw new PasswordRequirementsException();
+		}
+		log.info("User registered successfully");
+		log.info("Hashing password");
+		info.getUser().setPassword(info.getPassword());   //hashing will be done in setPassword()
+		return userService.add(info.getUser());
+	}
+}
